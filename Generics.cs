576e72c1489b82351d8c71adc0fc5202dac82b97
SnehaAkkenapally <sneha.akkenapally11@gmail.com>
2024-10-11 12:13:08 -0400

    Add files via upload

---
+++ Generics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGenerics
+{
+    public class Generics<T>
+    {
+        List<T> elements = new List<T>();
+
+        // Method to return the number of elements in the stack
+        public int Count()
+        {
+            return elements.Count;
+        }
+
+        // Method to add an element onto the stack
+        public void Push(T item)
+        {
+            elements.Add(item);
+        }
+
+        // Method to remove and return the top element from the stack
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            // Get the last item (top of the stack)
+            T item = elements[elements.Count - 1];
+
+            // Remove the last item
+            elements.RemoveAt(elements.Count - 1);
+
+            return item;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleGenerics;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+Generics<int> intStack = new Generics<int>();
+
+// Push items onto the stack
+intStack.Push(10);
+intStack.Push(20);
+intStack.Push(30);
+
+// Display the count of items in the stack
+Console.WriteLine("Count: " + intStack.Count());
+
+// Pop items from the stack
+Console.WriteLine("Popped: " + intStack.Pop());
+Console.WriteLine("Popped: " + intStack.Pop());
+
+// Display the count after popping
+Console.WriteLine("Count after popping: " + intStack.Count());
